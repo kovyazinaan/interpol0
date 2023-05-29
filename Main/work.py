b0b4f62923da5767be0e4a13de0ca197e1dfa96c
aeltsovaa <aelcova63@gmail.com>
2023-05-29 14:55:46 +0300

    первая часть

---
+++ Main/work.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import openpyxl
+
+def merge_excel_files(file_paths, output_folder_path):
+    last_id = 0
+    legth_check = 0
+    for i, file_path in enumerate(file_paths):
+        df = pd.read_excel('Table/' + file_path)
+        if i == 0:
+            last_id = df.iloc[0]['id']
+        else:    
+            last_id += length_check
+        df['id'] = range(last_id + 1,last_id + len(df) + 1)
+        output_file_path = f'{output_folder_path}/new_table{i+1}.csv'
+        df.to_csv(output_file_path, index=False)
+        length_check = len(df)
