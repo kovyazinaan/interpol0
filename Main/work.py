3e75c55966d90316b041adc5cb5e7b67bff51cd1
kovyazinaan <kovyazina.a.n@gmail.com>
2023-05-29 14:58:54 +0300

    Добавила вывод функции

--- Main/work.py
+++ Main/work.py
@@ -14,3 +14,7 @@ def merge_excel_files(file_paths, output_folder_path):
         output_file_path = f'{output_folder_path}/new_table{i+1}.csv'
         df.to_csv(output_file_path, index=False)
         length_check = len(df)
+
+file_paths = ['1.xlsx', '2.xlsx', '3.xlsx']
+output_folder_path = 'Table'
+merge_excel_files(file_paths, output_folder_path)
